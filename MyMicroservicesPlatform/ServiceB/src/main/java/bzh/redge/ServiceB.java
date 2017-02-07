716ac8ad436b6c9d566b227093cf67294e9dc032
regis <regis@DESKTOP-L65G0E1>
2017-02-07 22:52:24 +0100

    Activayion d'hystrix

--- MyMicroservicesPlatform/ServiceB/src/main/java/bzh/redge/ServiceB.java
+++ MyMicroservicesPlatform/ServiceB/src/main/java/bzh/redge/ServiceB.java
@@ -3,11 +3,17 @@ package bzh.redge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 @EnableAutoConfiguration
+@EnableDiscoveryClient
+@ComponentScan
 public class ServiceB {
 
 	public static void main(String[] args) {

110317f094d948794f53000a952710c325f244b5
regis <regis@eureka2>
2017-02-07 12:16:47 +0100

    Creation des launch pour les diff instance du service A

--- MyMicroservicesPlatform/ServiceRegistration/src/main/java/bzh/redge/ServiceRegistrationApplication.java
+++ MyMicroservicesPlatform/ServiceRegistration/src/main/java/bzh/redge/ServiceRegistrationApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class MyServiceRegistrationApplication {
+public class ServiceRegistrationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyServiceRegistrationApplication.class, args);
+		SpringApplication.run(ServiceRegistrationApplication.class, args);
 	}
 }
--- MyMicroservicesPlatform/ServiceRegistration/src/test/java/bzh/redge/ServiceRegistrationApplicationTests.java
+++ MyMicroservicesPlatform/ServiceRegistration/src/test/java/bzh/redge/ServiceRegistrationApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MyServiceRegistrationApplicationTests {
+public class ServiceRegistrationApplicationTests {
 
 	@Test
 	public void contextLoads() {
